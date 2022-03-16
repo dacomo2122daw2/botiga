3e04b04855885167de214d38dd3ca2ed1586e259
vagrant <vagrant@laravel>
2022-03-16 18:31:46 +0000

    Commit 4 del projecte botiga

--- botiga/routes/web.php
+++ botiga/routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::resource('compra',ControladorCompra::class);
+
+Route::get('/autentica','LoginCompraControlador@autentica')->name('autenticacio');
